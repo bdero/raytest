ae87fde5aa3e2b5e0461cfb4931e61051319ddca
Brandon DeRosier <x@bdero.me>
2020-09-27 02:12:44 -0700

    Build LUA+Sol2

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,38 @@
+extern "C" {
+    #include <lua.h>
+}
+#include "sol/sol.hpp"
 #include "raylib.h"
 
+struct LuaThing {
+    float something = 0;
+};
+
+void luaTest() {
+    sol::state lua;
+    lua.open_libraries(sol::lib::base);
+
+    lua.script("print('Test print from the LUA VM.')");
+
+    int important = 0;
+    auto doThings = [&important]{
+      printf("Important LUA business: %d\n", important);
+      important++;
+    };
+    lua.set_function("do_things", doThings);
+    lua.script("do_things()\ndo_things()\ndo_things()");
+
+    lua.new_usertype<LuaThing>("Thing", "something", &LuaThing::something);
+    lua.script(
+            "thing = Thing.new()\n"
+            "thing.something = 420.69");
+    auto thing = lua.get<LuaThing>("thing");
+    printf("Important values from LUA: %.2f\n", thing.something);
+}
+
 int main() {
+    luaTest();
+
     SetConfigFlags(FLAG_WINDOW_RESIZABLE);
     InitWindow(800, 600, "Test window");
 
